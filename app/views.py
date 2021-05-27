73876a46c8f2dd5c37f59f501782d12e019c5317
ViniciussAlencarr <vinicius.alencaarr@gmail.com>
2021-05-27 02:17:50 -0300

    exibindo resultado do back-end no front-end (arquivo json da requisiçao)

--- app/views.py
+++ app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
 from app.forms import  *
+import requests
+import server
 
 # Create your views here.
 def homeScreen(request):
     data = {}
     data['form'] = ConsultaCepForm(request.POST)
     data['db'] = ConsultaCep.objects.all()
+    data['request'] = server.aux
     if data['form'].is_valid():
         data['form'].save()
     return render(request, 'index.html', data)
  
+
+
 def create(request):
     form = ConsultaCepForm(request.POST)
     if form.is_valid():
--- server.py
+++ server.py
@@ -2,9 +2,6 @@ import requests
 
 cep = '04855440'
 
-if len(cep) == 0:
-    print('Quantidade de digitos invvlída!')
-    exit()
 request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
 
-print(request.json())
+aux = request.json()
